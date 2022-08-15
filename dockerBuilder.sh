027876811f7495583ec93b25f1b73e965a2d48d4
jasper200207 <jasper12394@gmail.com>
2022-08-15 05:57:46 +0000

    set fastapi

--- dockerBuilder.sh
+++ dockerBuilder.sh
@@ -1,21 +1,19 @@
-CONTAINER_LIST=("nginx","1987_backend","1987_ai","1987_db")
-DB_CONTAINER_NAME=1987_db
-SQL_PATH=/etc/mysql/setDB/setDB.sql
-DB_ROOT_ID=root
-DB_ROOT_PASSWORD=qwer1234
+CONTAINER_LIST="nginx 1987-backend 1987-ai 1987-db"
 
-for name in "${CONTAINER_LIST[*]}"
+for name in $CONTAINER_LIST
 do
   echo "REMOVE '$name' "
   docker stop $name;
   docker rm $name;
-  docker rmi $name;
+  docker rmi 1987-infra_$name;
 done
+
+echo "Download Yolov3-spp weights"
+rm -R ./yolov3-spp.weights*
+wget https://pjreddie.com/media/files/yolov3-spp.weights
+mv ./yolov3-spp.weights ./1987-ai/yolov3-spp.weights
 
 echo "RUN Docker-compose"
 docker-compose up -d
-
-echo "Set DB"
-docker exec $DB_CONTAINER_NAME sh -c "mysql -u$DB_ROOT_ID -p$DB_ROOT_PASSWORD < $SQL_PATH"
 
 echo "Complete!"
