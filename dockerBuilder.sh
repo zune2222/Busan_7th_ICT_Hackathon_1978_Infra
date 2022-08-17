c740a72eedf7f70ca54882169d9d982eeb77d334
jasper200207 <jasper12394@gmail.com>
2022-08-17 14:48:56 +0000

    fix backend docker

--- dockerBuilder.sh
+++ dockerBuilder.sh
@@ -1,19 +1,34 @@
 CONTAINER_LIST="nginx 1987-backend 1987-ai 1987-db"
+DB_CONTAINER_NAME=1987-db
+SQL_PATH=/etc/mysql/setDB/setDB.sql
+DB_ROOT_ID=root
+DB_ROOT_PASSWORD=qwer1234
 
 for name in $CONTAINER_LIST
 do
-  echo "REMOVE '$name' "
+  echo "Stop Container '$name' "
   docker stop $name;
+  echo "Remove Container '$name' "
   docker rm $name;
+  echo "Remove Image '1987-infra_$name' "
   docker rmi 1987-infra_$name;
 done
 
-echo "Download Yolov3-spp weights"
+echo "Remove remain Yolov3-spp weights"
 rm -R ./yolov3-spp.weights*
+echo "DownLoad Yolov3-spp weights"
 wget https://pjreddie.com/media/files/yolov3-spp.weights
+echo "Move Yolov3-spp weights"
 mv ./yolov3-spp.weights ./1987-ai/yolov3-spp.weights
 
 echo "RUN Docker-compose"
 docker-compose up -d
+
+echo "Restart DB"
+docker restart $DB_CONTAINER_NAME
+
+echo "Set DB"
+sleep 10
+docker exec $DB_CONTAINER_NAME sh -c "mysql -u$DB_ROOT_ID < $SQL_PATH"
 
 echo "Complete!"
